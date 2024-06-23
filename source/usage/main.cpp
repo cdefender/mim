ef0dbe86bc02691103e11de6146b3c2ca08b3a59
cdefender <0xc3phalex1n@gmail.com>
2024-06-23 15:47:01 -0400

    Update - usage/main.cpp: Updated to some info.

--- source/usage/main.cpp
+++ source/usage/main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char* argv[])
 {
+	std::cout << "Morse Decoder/Encoder Written by Ai000 Cybernetics QLab" << std::endl;
 	std::string name = "-- ^.. ^.-.. ^.- ^-.. / -.- ^.- ^.... ^... ^.- ^.-. ^.. / .- ^.-.. ^.... ^.- ^-.. ^..";
 	std::cout << MorseCodeToPlaintext(name) << std::endl;
 
